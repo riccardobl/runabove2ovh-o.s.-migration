9859119448a5052aaa56181b2598a01a91abe9f6
Riccardo Balbo <riccardo@forkforge.net>
2015-10-11 13:40:03 +0200

    Better compare

--- ra2ovh_objectStorage.sh
+++ ra2ovh_objectStorage.sh
@@ -1,3 +1,8 @@
+default="\e[0m"
+red="\e[31m"
+green="\e[32m"
+blue="\e[34m"
+
 function usage {
     echo "[!] Usage: ./ra2ovh_objectStorage.sh [operation]"
     echo "[!] Operations:"
@@ -38,19 +43,38 @@ ra
 CONTAINERS=`swift list`
 while read -r container
 do 
-    echo "[!] Container: $container"
+    echo -e "[!] Container: $blue$container$default"
     if [ "$1" == "compare" ]
     then
         ra
         ra_objs=`swift list $container`
+        mapfile -t ra_objs <<< "$ra_objs"
         ovh
-        ovh_objs=`swift list $container`
-        if [ "$ra_objs" = "$ovh_objs" ]
+        ovh_objs==`swift list $container`
+        mapfile -t ovh_objs <<< "$ovh_objs"
+        sync=1
+        for o in "${ra_objs[@]}"
+        do
+            found=0
+            for o2 in "${ovh_objs[@]}"
+            do
+                if [ "$o" == "$o2" ]
+                then
+                   found=1
+                   break
+                fi
+            done
+            if [ $found == 0 ]
+            then
+                sync=0
+                echo -e "[-] $red'$o'$default is missing in OVH:$blue$container$default"
+            fi            
+        done
+        if [ $sync == 1 ] 
         then
-            echo "[+] Synchronized"
-        else
-            echo "[-] Not synchronized"
-        fi       
+            echo -e "[+]$green Synchronized! $default"
+        fi
+      
     elif [ "$1" == "sync" ]
     then
         sharedKey=$(openssl rand -base64 32) 
@@ -59,7 +83,7 @@ do
         destContainer=$(swift --debug stat $container 2>&1 | grep 'curl -i.*storage' | awk '{ print $4 }') 
         ra
         swift post --sync-key "$sharedKey" --sync-to "$destContainer" $container     
-        echo "[+] Sync from RA:$container to OVH:$destContainer"
+        echo -e "[+] Sync from RA:$blue$container$default to OVH:$blue$container$default"
     else
         usage
     fi
